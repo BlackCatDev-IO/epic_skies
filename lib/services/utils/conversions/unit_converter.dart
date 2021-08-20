49d5ed20a67c1a0dbf9280bd36d0d9a7916d690f
Loren Aguey <nunya@gmail.com>
2021-08-20 16:47:07 -0700

    made all params in Unit converter required

--- lib/services/utils/conversions/unit_converter.dart
+++ lib/services/utils/conversions/unit_converter.dart
@@ -3,16 +3,17 @@ import 'package:get/get.dart';
 class UnitConverter {
   static int toCelcius(int temp) => ((temp - 32) * 5 / 9).round();
 
-  static double convertFeetPerSecondToMph(num feet) =>
-      (feet / 1.467).toDouble().toPrecision(1);
+  static double convertFeetPerSecondToMph({required num feetPerSecond}) =>
+      (feetPerSecond / 1.467).toDouble().toPrecision(1);
 
-  static double convertInchesToMillimeters(num? inches) {
+  static double convertInchesToMillimeters({required num inches}) {
     if (inches == 0.0 || inches == 0) {
       return 0;
     } else {
-      return (inches! * 25.4).toDouble().toPrecision(2);
+      return (inches * 25.4).toDouble().toPrecision(2);
     }
   }
 
-  static int convertMilesToKph(num miles) => (miles * 1.609344).round();
+  static int convertMilesToKph({required num miles}) =>
+      (miles * 1.609344).round();
 }
