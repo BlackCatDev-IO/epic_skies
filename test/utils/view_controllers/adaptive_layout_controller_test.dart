f0e1d253b36d41d1a627bdf58f4a319e0a195445
Loren Aguey <loren@blackcatdev.io>
2021-11-01 22:31:24 +0000

    REFACTOR: create dedicated folder for test mocks

--- test/utils/view_controllers/adaptive_layout_controller_test.dart
+++ test/utils/view_controllers/adaptive_layout_controller_test.dart
@@ -7,7 +7,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:get/get.dart';
 import 'package:path_provider_platform_interface/path_provider_platform_interface.dart';
 
-import '../test_utils.dart';
+import '../../test_utils.dart';
 
 void main() {
   final TestWidgetsFlutterBinding binding =
