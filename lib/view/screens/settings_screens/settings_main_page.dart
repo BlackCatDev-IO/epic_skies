5e5854211723f43da17bdd19fedf291852e32a91
Loren Aguey <loren@blackcatdev.io>
2021-11-06 23:02:55 +0000

    FIX: Android back button from settings main page goes home instead of killing app

--- lib/view/screens/settings_screens/settings_main_page.dart
+++ lib/view/screens/settings_screens/settings_main_page.dart
@@ -1,5 +1,6 @@
 import 'package:black_cat_lib/black_cat_lib.dart';
 import 'package:epic_skies/global/local_constants.dart';
+import 'package:epic_skies/services/ticker_controllers/drawer_animation_controller.dart';
 import 'package:epic_skies/view/widgets/buttons/home_from_settings_button.dart';
 import 'package:epic_skies/view/widgets/image_widget_containers/weather_image_container.dart';
 import 'package:flutter/material.dart';
@@ -15,57 +16,63 @@ import 'units_screen.dart';
 class SettingsMainPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return FixedImageContainer(
-      image: earthFromSpace,
-      child: Column(
-        children: [
-          const SettingsHeader(title: 'Settings', backButtonShown: false),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              ListView(
-                padding: EdgeInsets.zero,
-                children: [
-                  const HomeFromSettingsButton(),
-                  // SettingsTile(
-                  //     title: 'Notifications',
-                  //     onPressed: () {},
-                  //     icon: Icons.alarm),
-                  SettingsTile(
-                    title: 'Unit Settings',
-                    onPressed: () => Get.toNamed(UnitsScreen.id),
-                    icon: Icons.thermostat,
-                  ),
-                  SettingsTile(
-                    title: 'Background Image Settings',
-                    onPressed: () => Get.toNamed(BgImageSettingsScreen.id),
-                    icon: Icons.add_a_photo,
-                  ),
-                  // SettingsTile(
-                  //     title: 'Image & Icon Credits',
-                  //     onPressed: () => Get.toNamed(ImageCreditScreen.id),
-                  //     icon: Icons.photo),
-                  SettingsTile(
-                    title: 'Contact',
-                    onPressed: () async {
-                      final email = Email(
-                        subject: 'Epic Skies Feedback',
-                        recipients: [myEmail],
-                      );
-                      await FlutterEmailSender.send(email);
-                    },
-                    icon: Icons.email,
-                  ),
-                  SettingsTile(
-                    title: 'About',
-                    onPressed: () => Get.toNamed(AboutPage.id),
-                    icon: Icons.info,
-                  )
-                ],
-              ).expanded(),
-            ],
-          ).paddingSymmetric(horizontal: 5).expanded(),
-        ],
+    return WillPopScope(
+      onWillPop: () async {
+        DrawerAnimationController.to.navigateToHome();
+        return false;
+      },
+      child: FixedImageContainer(
+        image: earthFromSpace,
+        child: Column(
+          children: [
+            const SettingsHeader(title: 'Settings', backButtonShown: false),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                ListView(
+                  padding: EdgeInsets.zero,
+                  children: [
+                    const HomeFromSettingsButton(),
+                    // SettingsTile(
+                    //     title: 'Notifications',
+                    //     onPressed: () {},
+                    //     icon: Icons.alarm),
+                    SettingsTile(
+                      title: 'Unit Settings',
+                      onPressed: () => Get.toNamed(UnitsScreen.id),
+                      icon: Icons.thermostat,
+                    ),
+                    SettingsTile(
+                      title: 'Background Image Settings',
+                      onPressed: () => Get.toNamed(BgImageSettingsScreen.id),
+                      icon: Icons.add_a_photo,
+                    ),
+                    // SettingsTile(
+                    //     title: 'Image & Icon Credits',
+                    //     onPressed: () => Get.toNamed(ImageCreditScreen.id),
+                    //     icon: Icons.photo),
+                    SettingsTile(
+                      title: 'Contact',
+                      onPressed: () async {
+                        final email = Email(
+                          subject: 'Epic Skies Feedback',
+                          recipients: [myEmail],
+                        );
+                        await FlutterEmailSender.send(email);
+                      },
+                      icon: Icons.email,
+                    ),
+                    SettingsTile(
+                      title: 'About',
+                      onPressed: () => Get.toNamed(AboutPage.id),
+                      icon: Icons.info,
+                    )
+                  ],
+                ).expanded(),
+              ],
+            ).paddingSymmetric(horizontal: 5).expanded(),
+          ],
+        ),
       ),
     );
   }
