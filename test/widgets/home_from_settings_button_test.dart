c7e4ef99c4cbe6d51a1dd53943c0e1f43aee8369
Loren Aguey <loren@blackcatdev.io>
2021-11-30 09:40:14 -0500

    TEST: add HomeFromSettingsButton test

--- test/widgets/home_from_settings_button_test.dart
+++ test/widgets/home_from_settings_button_test.dart
@@ -1,6 +1,8 @@
-import 'package:epic_skies/services/utils/view_controllers/view_controller.dart';
-import 'package:epic_skies/view/widgets/general/buttons/home_from_settings_button.dart';
-import 'package:epic_skies/view/widgets/general/settings_widgets/settings_list_tile.dart';
+import 'package:epic_skies/services/ticker_controllers/drawer_animation_controller.dart';
+import 'package:epic_skies/services/ticker_controllers/tab_navigation_controller.dart';
+import 'package:epic_skies/view/widgets/buttons/home_from_settings_button.dart';
+import 'package:epic_skies/view/widgets/settings_widgets/settings_list_tile.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:get/get.dart';
 
@@ -8,23 +10,67 @@ import '../test_utils.dart';
 
 void main() {
   setUp(() {
-    Get.put(ViewController());
+    Get.put(DrawerAnimationController());
+    Get.put(TabNavigationController());
   });
-  testWidgets('HomeFromSettingsButton', (WidgetTester tester) async {
-    await tester.pumpWidget(
-      const MaterialWidgetTestAncestorWidget(
-        child: HomeFromSettingsButton(),
-      ),
-    );
-    final tile = find.byType(SettingsTile);
-    expect(tile, findsOneWidget);
-    expect(find.text('Home'), findsOneWidget);
-    await tester.tap(tile);
 
-    /// verifying home tab
-    expect(ViewController.to.tabController.index, 0);
+  group('HomeFromSettingsButton test', () {
+    testWidgets('Display "Home" and home icon and arrow icon',
+        (WidgetTester tester) async {
+      await tester.pumpWidget(
+        const MaterialWidgetTestAncestorWidget(
+          child: HomeFromSettingsButton(),
+        ),
+      );
+      expect(find.text('Home'), findsOneWidget);
 
-    /// verify animation controller reversed to 0.0
-    expect(ViewController.to.animationController.value, 0.0);
+      expect(find.byIcon(Icons.home), findsOneWidget);
+      expect(find.byIcon(Icons.chevron_right), findsOneWidget);
+    });
+
+    testWidgets('finds "Home" text', (WidgetTester tester) async {
+      await tester.pumpWidget(
+        const MaterialWidgetTestAncestorWidget(
+          child: HomeFromSettingsButton(),
+        ),
+      );
+      expect(find.text('Home'), findsOneWidget);
+    });
+
+    testWidgets('finds SettingsTile', (WidgetTester tester) async {
+      await tester.pumpWidget(
+        const MaterialWidgetTestAncestorWidget(
+          child: HomeFromSettingsButton(),
+        ),
+      );
+      final tile = find.byType(SettingsTile);
+      expect(tile, findsOneWidget);
+    });
+
+    testWidgets('Navigates to Home Screen on tap', (WidgetTester tester) async {
+      await tester.pumpWidget(
+        const MaterialWidgetTestAncestorWidget(
+          child: HomeFromSettingsButton(),
+        ),
+      );
+
+      await tester.runAsync(() async {
+        await tester.pumpWidget(
+          const MaterialWidgetTestAncestorWidget(
+            child: HomeFromSettingsButton(),
+          ),
+        );
+
+        await tester.tap(find.byType(HomeFromSettingsButton));
+        final tile = find.byType(SettingsTile);
+        expect(tile, findsOneWidget);
+
+        /// verifying home tab
+        expect(TabNavigationController.to.tabController.index, 0);
+
+        /// verify animation controller reversed to 0.0
+        expect(DrawerAnimationController.to.animationController.value, 0.0);
+      });
+    });
   });
 }
