79603ffdc7390090b04abfe56d5a55a9bd801487
TheDudeAbides1984 <lorenaguey@gmail.com>
2022-02-19 22:09:07 -0500

    FIX: prevent unneccesary builds on scroll of DailyForecastPage

--- lib/services/view_controllers/scroll_position_controller.dart
+++ lib/services/view_controllers/scroll_position_controller.dart
@@ -1,5 +1,7 @@
+import 'dart:developer';
+
+import 'package:epic_skies/features/daily_forecast/controllers/daily_forecast_controller.dart';
 import 'package:epic_skies/services/ticker_controllers/tab_navigation_controller.dart';
-import 'package:epic_skies/services/weather_forecast/daily_forecast_controller.dart';
 import 'package:get/get.dart';
 import 'package:scrollable_positioned_list/scrollable_positioned_list.dart';
 
@@ -10,7 +12,7 @@ class ScrollPositionController extends GetxController {
   ItemPositionsListener itemPositionsListener = ItemPositionsListener.create();
 
   int selectedDayIndex = 0;
-  int updatedDayIndex = 0;
+  int currentIndex = 0;
 
   /// prevents scrollAfterFirstBuild from running if user didn't navigate
   /// to Daily tab from Home tab
@@ -20,7 +22,17 @@ class ScrollPositionController extends GetxController {
   void onInit() {
     super.onInit();
     itemPositionsListener.itemPositions.addListener(() {
-      _updateSelectedDailyNavButtonOnScroll();
+      final itemLeadingEdge =
+          itemPositionsListener.itemPositions.value.first.itemLeadingEdge;
+
+      final listenerIndex =
+          itemPositionsListener.itemPositions.value.first.index;
+
+      /// Prevents this from getting called hundreds of times
+      /// as user scrolls
+      if (itemLeadingEdge != 0.0 && listenerIndex != selectedDayIndex) {
+        _updateSelectedDailyNavButtonOnScroll(itemLeadingEdge);
+      }
     });
   }
 
@@ -31,10 +43,11 @@ class ScrollPositionController extends GetxController {
   /// won't work because it will have had nothing to attach to
   /// This will not run if user jumps to Daily tab from TabBar the first time
   void scrollAfterFirstBuild() {
+    log('scroll after first build selectedIndex: $selectedDayIndex');
     scrollToIndex(index: selectedDayIndex);
   }
 
-  void scrollToIndex({required int index}) {
+  Future<void> scrollToIndex({required int index}) async {
     selectedDayIndex = index;
 
     if (itemScrollController.isAttached) {
@@ -42,21 +55,27 @@ class ScrollPositionController extends GetxController {
         index: selectedDayIndex,
       );
     }
+
+    currentIndex = selectedDayIndex;
   }
 
   Future<void> jumpToDayFromHomeScreen({required int index}) async {
     navigateToDailyTabFromHome = true;
     selectedDayIndex = index;
-    await TabNavigationController.to.jumpToTab(index: 2);
     scrollToIndex(index: selectedDayIndex);
-    DailyForecastController.to.updateSelectedDayStatus(index: selectedDayIndex);
+    DailyForecastController.to
+        .updateSelectedDayStatus(newIndex: selectedDayIndex);
+
+    TabNavigationController.to.jumpToTab(index: 2);
   }
 
-  void _updateSelectedDailyNavButtonOnScroll() {
-    final currentIndex = itemPositionsListener.itemPositions.value.first.index;
-    if (currentIndex != selectedDayIndex) {
-      DailyForecastController.to.updateSelectedDayStatus(index: currentIndex);
+  void _updateSelectedDailyNavButtonOnScroll(double itemLeadingEdge) {
+    final listenerIndex = itemPositionsListener.itemPositions.value.first.index;
+
+    if (listenerIndex != selectedDayIndex && itemLeadingEdge != 0.0) {
+      DailyForecastController.to.updateSelectedDayStatus(
+        newIndex: listenerIndex,
+      );
     }
-    selectedDayIndex = currentIndex;
   }
 }
