0989d604be24d148ac43b10c5196fd763371dfaf
Loren Aguey <lorenaguey@Lorens-MacBook-Pro-2.local>
2023-01-04 18:49:44 -0500

    replace UnitSettingsController with UnitSettingsBloc. (#40)

--- lib/services/settings/unit_settings/unit_settings_model.dart
+++ lib/services/settings/unit_settings/unit_settings_model.dart
@@ -1,11 +1,12 @@
 import 'dart:convert';
 
+import 'package:equatable/equatable.dart';
 import 'package:objectbox/objectbox.dart';
 
 @Entity()
-class UnitSettings {
-  UnitSettings({
-    required this.id,
+class UnitSettings extends Equatable {
+  const UnitSettings({
+    this.id = 1,
     required this.tempUnitsMetric,
     required this.timeIn24Hrs,
     required this.precipInMm,
@@ -14,11 +15,11 @@ class UnitSettings {
 
   /// only one global unit settings object so id will always be 1
   @Id(assignable: true)
-  int id;
-  bool tempUnitsMetric;
-  bool timeIn24Hrs;
-  bool precipInMm;
-  bool speedInKph;
+  final int id;
+  final bool tempUnitsMetric;
+  final bool timeIn24Hrs;
+  final bool precipInMm;
+  final bool speedInKph;
 
   String toRawJson() {
     final map = {
@@ -36,11 +37,32 @@ class UnitSettings {
     final map = json.decode(rawJson) as Map;
 
     return UnitSettings(
-      id: 1,
       tempUnitsMetric: map['tempUnitsMetric'] as bool,
       timeIn24Hrs: map['timeIn24Hrs'] as bool,
       precipInMm: map['precipInMm'] as bool,
       speedInKph: map['speedInKph'] as bool,
     );
   }
+
+  UnitSettings copyWith({
+    bool? tempUnitsMetric,
+    bool? timeIn24Hrs,
+    bool? precipInMm,
+    bool? speedInKph,
+  }) {
+    return UnitSettings(
+      tempUnitsMetric: tempUnitsMetric ?? this.tempUnitsMetric,
+      timeIn24Hrs: timeIn24Hrs ?? this.timeIn24Hrs,
+      precipInMm: precipInMm ?? this.precipInMm,
+      speedInKph: speedInKph ?? this.speedInKph,
+    );
+  }
+
+  @override
+  List<Object?> get props => [
+        tempUnitsMetric,
+        timeIn24Hrs,
+        precipInMm,
+        speedInKph,
+      ];
 }
