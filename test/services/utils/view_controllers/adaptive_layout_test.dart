f6038f1cbf51cf2ecfd22f6e9c2fd29b9b08c87e
Loren Aguey <loren@blackcatdev.io>
2023-01-15 11:52:28 -0500

    test: update AdaptiveLayout test. (#48)

---
+++ test/services/utils/view_controllers/adaptive_layout_test.dart
@@ -0,0 +1,83 @@
+import 'package:epic_skies/services/view_controllers/adaptive_layout.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import '../../../test_utils.dart';
+
+void main() {
+  final TestWidgetsFlutterBinding binding =
+      TestWidgetsFlutterBinding.ensureInitialized();
+
+  late AdaptiveLayout adaptiveLayout;
+  late AdaptiveLayout adaptiveLayoutWithNotch;
+
+  setUpAll(() async {
+    adaptiveLayout = AdaptiveLayout(hasNotch: false);
+    adaptiveLayoutWithNotch = AdaptiveLayout(hasNotch: true);
+  });
+  group('Set adaptive screen sizes:', () {
+    test('Values for phones without notch', () {
+      adaptiveLayout.setAdaptiveHeights();
+      expect(adaptiveLayout.appBarPadding, 19.5);
+      expect(adaptiveLayout.appBarHeight, 19);
+      expect(adaptiveLayout.settingsHeaderHeight, 19);
+    });
+
+    testWidgets('Values for iPhones with notch over 897 pix height',
+        (tester) async {
+      binding.window.physicalSizeTestValue = const Size(400, 900);
+      binding.window.devicePixelRatioTestValue = 1.0;
+
+      await tester
+          .pumpWidget(MaterialWidgetTestAncestorWidget(child: Container()));
+
+      adaptiveLayoutWithNotch.setAdaptiveHeights();
+      expect(adaptiveLayoutWithNotch.appBarPadding, 19.5);
+      expect(adaptiveLayoutWithNotch.appBarHeight, 14);
+      expect(adaptiveLayoutWithNotch.settingsHeaderHeight, 19);
+    });
+
+    testWidgets('Values for iPhones with between 870 and 896 pix height',
+        (tester) async {
+      binding.window.physicalSizeTestValue = const Size(400, 880);
+      binding.window.devicePixelRatioTestValue = 1.0;
+
+      await tester
+          .pumpWidget(MaterialWidgetTestAncestorWidget(child: Container()));
+
+      adaptiveLayoutWithNotch.setAdaptiveHeights();
+      expect(adaptiveLayoutWithNotch.appBarHeight, 15);
+      expect(adaptiveLayoutWithNotch.appBarPadding, 20.5);
+      expect(adaptiveLayoutWithNotch.settingsHeaderHeight, 19);
+    });
+
+    testWidgets('Values for iPhones with between 800 and 869 pix height',
+        (tester) async {
+      binding.window.physicalSizeTestValue = const Size(400, 850);
+      binding.window.devicePixelRatioTestValue = 1.0;
+
+      await tester
+          .pumpWidget(MaterialWidgetTestAncestorWidget(child: Container()));
+
+      adaptiveLayoutWithNotch.setAdaptiveHeights();
+      expect(adaptiveLayoutWithNotch.appBarHeight, 14.5);
+      expect(adaptiveLayoutWithNotch.appBarPadding, 21);
+      expect(adaptiveLayoutWithNotch.settingsHeaderHeight, 18);
+    });
+
+    testWidgets('Values for phones with notch under 800 pix height',
+        (tester) async {
+      binding.window.physicalSizeTestValue = const Size(400, 750);
+      binding.window.devicePixelRatioTestValue = 1.0;
+
+      await tester
+          .pumpWidget(MaterialWidgetTestAncestorWidget(child: Container()));
+
+      adaptiveLayoutWithNotch.setAdaptiveHeights();
+
+      expect(adaptiveLayoutWithNotch.appBarHeight, 14);
+      expect(adaptiveLayoutWithNotch.appBarPadding, 20.5);
+      expect(adaptiveLayoutWithNotch.settingsHeaderHeight, 18);
+    });
+  });
+}
