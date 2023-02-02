0fb5409e8ab5108ea7b59b372a2d56bac0a59796
Loren Aguey <loren@blackcatdev.io>
2023-02-01 19:23:40 -0500

    test: update About Settings Tile widget test (#48)

--- test/view/widgets/settings_widgets/about_widget_test.dart
+++ test/view/widgets/settings_widgets/about_widget_test.dart
@@ -1,12 +1,8 @@
-import 'package:epic_skies/services/app_updates/update_controller.dart';
-import 'package:epic_skies/services/view_controllers/adaptive_layout_controller.dart';
 import 'package:epic_skies/view/screens/settings_screens/about_screen.dart';
 import 'package:epic_skies/view/widgets/settings_widgets/settings_list_tile.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:get/get.dart';
 
-import '../../../mocks/mock_classes.dart';
 import '../../../test_utils.dart';
 
 class _MockAboutButton extends StatelessWidget {
@@ -14,37 +10,13 @@ class _MockAboutButton extends StatelessWidget {
   Widget build(BuildContext context) {
     return SettingsTile(
       title: 'About',
-      onPressed: () => Get.toNamed(AboutPage.id),
+      onPressed: () => Navigator.of(context).pushNamed(AboutPage.id),
       icon: Icons.info,
     );
   }
 }
 
 void main() {
-  late MockStorageController mockStorage;
-  late AdaptiveLayoutController adaptiveLayoutController;
-  late UpdateController updateController;
-
-  setUpAll(() {
-    mockStorage = MockStorageController();
-    updateController = UpdateController(mockStorage);
-    Get.put(updateController);
-    adaptiveLayoutController =
-        AdaptiveLayoutController(storage: mockStorage, hasNotch: false);
-    Get.put(adaptiveLayoutController);
-
-    adaptiveLayoutController.setAdaptiveHeights();
-
-    updateController.currentAppVersion = '0.1.1';
-  });
-
-  tearDownAll(() async {
-    await Future.wait([
-      Get.delete<UpdateController>(),
-      Get.delete<AdaptiveLayoutController>(),
-    ]);
-  });
-
   group('About Widget test', () {
     testWidgets('Display info icon and arrow icon',
         (WidgetTester tester) async {
@@ -82,7 +54,6 @@ void main() {
         await tester.pumpAndSettle();
 
         expect(find.byType(AboutPage), findsOneWidget);
-        expect(Get.currentRoute, AboutPage.id);
       });
     });
   });
