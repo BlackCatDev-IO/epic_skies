bb1802edd7e2ccbb984b070ef2ebed888c566f47
Loren Aguey <loren@blackcatdev.io>
2023-02-24 06:16:03 -0500

    show ad purchase/error dialogs from SettingsMainPage (#32)

--- lib/view/screens/settings_screens/settings_main_page.dart
+++ lib/view/screens/settings_screens/settings_main_page.dart
@@ -2,7 +2,9 @@ import 'dart:io';
 
 import 'package:black_cat_lib/black_cat_lib.dart';
 import 'package:epic_skies/extensions/widget_extensions.dart';
+import 'package:epic_skies/features/banner_ads/bloc/ad_bloc.dart';
 import 'package:epic_skies/global/local_constants.dart';
+import 'package:epic_skies/view/dialogs/ad_dialogs.dart';
 import 'package:epic_skies/view/screens/settings_screens/about_screen.dart';
 import 'package:epic_skies/view/screens/settings_screens/bg_settings_screen.dart';
 import 'package:epic_skies/view/screens/settings_screens/units_screen.dart';
@@ -12,6 +14,7 @@ import 'package:epic_skies/view/widgets/image_widget_containers/weather_image_co
 import 'package:epic_skies/view/widgets/settings_widgets/settings_header.dart';
 import 'package:epic_skies/view/widgets/settings_widgets/settings_list_tile.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_email_sender/flutter_email_sender.dart';
 
 class SettingsMainPage extends StatelessWidget {
@@ -25,53 +28,74 @@ class SettingsMainPage extends StatelessWidget {
       child: TextScaleFactorClamper(
         child: FixedImageContainer(
           imagePath: earthFromSpace,
-          child: Column(
-            children: [
-              SettingsHeader(
-                title: 'Settings',
-                backButtonShown: Platform.isIOS,
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  ListView(
-                    padding: EdgeInsets.zero,
-                    children: [
-                      const HomeFromSettingsButton(),
-                      SettingsTile(
-                        title: 'Unit Settings',
-                        onPressed: () =>
-                            Navigator.of(context).pushNamed(UnitsScreen.id),
-                        icon: Icons.thermostat,
-                      ),
-                      SettingsTile(
-                        title: 'Background Image Settings',
-                        onPressed: () => Navigator.of(context)
-                            .pushNamed(BgImageSettingsScreen.id),
-                        icon: Icons.add_a_photo,
-                      ),
-                      SettingsTile(
-                        title: 'Contact',
-                        onPressed: () async {
-                          final email = Email(
-                            subject: 'Epic Skies Feedback',
-                            recipients: [myEmail],
-                          );
-                          await FlutterEmailSender.send(email);
-                        },
-                        icon: Icons.email,
-                      ),
-                      SettingsTile(
-                        title: 'About',
-                        onPressed: () =>
-                            Navigator.of(context).pushNamed(AboutPage.id),
-                        icon: Icons.info,
-                      )
-                    ],
-                  ).expanded(),
-                ],
-              ).paddingSymmetric(horizontal: 5).expanded(),
-            ],
+          child: BlocListener<AdBloc, AdState>(
+            listener: (context, state) {
+              if (state.status.isError) {
+                AdDialogs.adPurchaseError(context, state.errorMessage);
+              }
+
+              if (state.status.isAdFreePurchased) {
+                AdDialogs.confirmAdPurchase(context);
+              }
+            },
+            child: Stack(
+              children: [
+                Column(
+                  children: [
+                    SettingsHeader(
+                      title: 'Settings',
+                      backButtonShown: Platform.isIOS,
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        ListView(
+                          padding: EdgeInsets.zero,
+                          children: [
+                            const HomeFromSettingsButton(),
+                            SettingsTile(
+                              title: 'Unit Settings',
+                              onPressed: () => Navigator.of(context)
+                                  .pushNamed(UnitsScreen.id),
+                              icon: Icons.thermostat,
+                            ),
+                            SettingsTile(
+                              title: 'Background Image Settings',
+                              onPressed: () => Navigator.of(context)
+                                  .pushNamed(BgImageSettingsScreen.id),
+                              icon: Icons.add_a_photo,
+                            ),
+                            SettingsTile(
+                              title: 'Contact',
+                              onPressed: () async {
+                                final email = Email(
+                                  subject: 'Epic Skies Feedback',
+                                  recipients: [myEmail],
+                                );
+                                await FlutterEmailSender.send(email);
+                              },
+                              icon: Icons.email,
+                            ),
+                            SettingsTile(
+                              title: 'About',
+                              onPressed: () =>
+                                  Navigator.of(context).pushNamed(AboutPage.id),
+                              icon: Icons.info,
+                            ),
+                            SettingsTile(
+                              title: 'Remove Ads',
+                              onPressed: () =>
+                                  AdDialogs.confirmAdPurchase(context),
+                              icon: Icons.sell,
+                            ),
+                          ],
+                        ).expanded(),
+                      ],
+                    ).paddingSymmetric(horizontal: 5).expanded(),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
