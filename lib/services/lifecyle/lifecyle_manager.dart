230a724559bb1350136d858d1d1865b9cc8ec75b
Loren Aguey <loren@blackcatdev.io>
2023-03-26 13:51:45 -0400

    add LifeCyleManager class

---
+++ lib/services/lifecyle/lifecyle_manager.dart
@@ -0,0 +1,54 @@
+import 'package:epic_skies/services/connectivity/connectivity_listener.dart';
+import 'package:epic_skies/utils/logging/app_debug_log.dart';
+import 'package:flutter/material.dart';
+
+class LifeCycleManager extends StatefulWidget {
+  const LifeCycleManager({super.key, required this.child});
+
+  final Widget child;
+
+  @override
+  State<LifeCycleManager> createState() => _LifeCycleManagerState();
+}
+
+class _LifeCycleManagerState extends State<LifeCycleManager>
+    with WidgetsBindingObserver {
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addObserver(this);
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    super.didChangeAppLifecycleState(state);
+
+    switch (state) {
+      case AppLifecycleState.resumed:
+        ConnectivityListener.initConnectivityListener();
+        break;
+      case AppLifecycleState.inactive:
+        break;
+      case AppLifecycleState.paused:
+      case AppLifecycleState.detached:
+        ConnectivityListener.disposeConnectivityListener();
+    }
+    _logLifecycleState(state);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return widget.child;
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    ConnectivityListener.disposeConnectivityListener();
+    super.dispose();
+  }
+
+  void _logLifecycleState(AppLifecycleState state) {
+    AppDebug.log('AppLifecycleState: $state', name: 'LifeCycleManager');
+  }
+}
