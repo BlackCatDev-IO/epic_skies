a96caf7a8726845c3481763132b8fbd9f20193ef
Loren Aguey <loren@blackcatdev.io>
2023-04-18 22:08:46 -0400

    Update AdDialogs to use platform dependent dialogs

--- lib/view/dialogs/ad_dialogs.dart
+++ lib/view/dialogs/ad_dialogs.dart
@@ -1,8 +1,5 @@
-import 'dart:io';
-
 import 'package:epic_skies/features/banner_ads/bloc/ad_bloc.dart';
-import 'package:epic_skies/global/app_theme.dart';
-import 'package:flutter/cupertino.dart';
+import 'package:epic_skies/view/dialogs/platform_dialog.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -18,166 +15,77 @@ Thanks for supporting the developer!
 
 Enjoy Epic Skies ad free 😎''';
 
-    final dialog = Platform.isIOS
-        ? CupertinoAlertDialog(
-            content: const Text(content, style: iOSContentTextStyle),
-            actions: [
-              CupertinoDialogAction(
-                onPressed: () => Navigator.of(context).pop(),
-                child: const Text('Cool!'),
-              ),
-            ],
-          )
-        : AlertDialog(
-            content: const Text(content),
-            actions: [
-              TextButton(
-                onPressed: () => Navigator.of(context).pop(),
-                child: const Text('Cool!', style: dialogActionTextStyle),
-              ),
-            ],
-          );
+    final actions = {
+      'Cool!': () => Navigator.of(context).pop(),
+    };
 
-    showDialog<void>(context: context, builder: (context) => dialog);
+    Dialogs.showPlatformDialog(
+      context,
+      content: content,
+      dialogActions: actions,
+    );
   }
 
   static void explainAdPolicy(BuildContext context) {
     const content =
         r'''Epic Skies will be ad free for 7 days. After that, you will see ads or you can remove ads by purchasing premium for a one time fee of $0.99''';
 
-    final dialog = Platform.isIOS
-        ? CupertinoAlertDialog(
-            content: const Text(content, style: iOSContentTextStyle),
-            actions: [
-              CupertinoDialogAction(
-                onPressed: () => Navigator.of(context).pop(),
-                child: const Text(
-                  'Got it!',
-                  style: TextStyle(fontSize: 22),
-                ),
-              ),
-            ],
-          )
-        : AlertDialog(
-            content: const Text(content),
-            actions: [
-              TextButton(
-                onPressed: () => Navigator.of(context).pop(),
-                child: const Text('Got it!', style: dialogActionTextStyle),
-              ),
-            ],
-          );
+    final actions = {
+      'Got it!': () => Navigator.of(context).pop(),
+    };
 
-    showDialog<void>(context: context, builder: (context) => dialog);
+    Dialogs.showPlatformDialog(
+      context,
+      content: content,
+      dialogActions: actions,
+    );
   }
 
   static void trialEnded(BuildContext context) {
     const content =
         r'''Thanks for using Epic Skies for 7 days! The ad free grace period has ended. You can remove ads permanently and support the developer for a one time fee of $0.99''';
 
-    const continueText = Text(
-      'Continue with ads',
-      style: TextStyle(color: Colors.red),
-    );
-    const purchaseText = Text('Purchase Premium');
-
     void popAndConfirmPurchase(BuildContext context) {
       Navigator.of(context).pop();
       confirmBeforeAdFreePurchase(context);
     }
 
-    final dialog = Platform.isIOS
-        ? CupertinoAlertDialog(
-            content: const Text(content, style: iOSContentTextStyle),
-            actions: [
-              CupertinoDialogAction(
-                onPressed: () => Navigator.of(context).pop(),
-                child: continueText,
-              ),
-              CupertinoDialogAction(
-                onPressed: () => popAndConfirmPurchase(context),
-                child: purchaseText,
-              ),
-            ],
-          )
-        : AlertDialog(
-            content: const Text(content),
-            actions: [
-              TextButton(
-                onPressed: () => Navigator.of(context).pop(),
-                child: continueText,
-              ),
-              TextButton(
-                onPressed: () => popAndConfirmPurchase(context),
-                child: purchaseText,
-              ),
-            ],
-          );
+    final actions = {
+      'Continue with ads': () => Navigator.of(context).pop(),
+      'Purchase Premium': () => popAndConfirmPurchase(context),
+    };
 
-    showDialog<void>(context: context, builder: (context) => dialog);
+    Dialogs.showPlatformDialog(
+      context,
+      content: content,
+      dialogActions: actions,
+    );
   }
 
   static void adPurchaseError(BuildContext context, String message) {
-    const purchaseText = Text('Ok');
+    final actions = {
+      'Ok': () => _purchaseAdFree(context),
+    };
 
-    final dialog = Platform.isIOS
-        ? CupertinoAlertDialog(
-            content: Text(message, style: iOSContentTextStyle),
-            actions: [
-              CupertinoDialogAction(
-                onPressed: () => _purchaseAdFree(context),
-                child: purchaseText,
-              ),
-            ],
-          )
-        : AlertDialog(
-            content: Text(message),
-            actions: [
-              TextButton(
-                onPressed: () => _purchaseAdFree(context),
-                child: purchaseText,
-              ),
-            ],
-          );
-
-    showDialog<void>(context: context, builder: (context) => dialog);
+    Dialogs.showPlatformDialog(
+      context,
+      content: message,
+      dialogActions: actions,
+    );
   }
 
   static void confirmBeforeAdFreePurchase(BuildContext context) {
-    const content =
-        r"Are you sure you'd like to remove ads for a one-time fee of  $0.99?";
+    const content = r'Remove ads for a one-time fee of  $0.99?';
 
-    const continueText = Text('No thanks', style: TextStyle(color: Colors.red));
-    const purchaseText = Text('Go for it!');
+    final actions = {
+      'No thanks': () => Navigator.of(context).pop(),
+      'Go for it!': () => _purchaseAdFree(context),
+    };
 
-    final dialog = Platform.isIOS
-        ? CupertinoAlertDialog(
-            content: const Text(content, style: iOSContentTextStyle),
-            actions: [
-              CupertinoDialogAction(
-                onPressed: () => Navigator.of(context).pop(),
-                child: continueText,
-              ),
-              CupertinoDialogAction(
-                onPressed: () => _purchaseAdFree(context),
-                child: purchaseText,
-              ),
-            ],
-          )
-        : AlertDialog(
-            content: const Text(content),
-            actions: [
-              TextButton(
-                onPressed: () => Navigator.of(context).pop(),
-                child: continueText,
-              ),
-              TextButton(
-                onPressed: () => _purchaseAdFree(context),
-                child: purchaseText,
-              ),
-            ],
-          );
-
-    showDialog<void>(context: context, builder: (context) => dialog);
+    Dialogs.showPlatformDialog(
+      context,
+      content: content,
+      dialogActions: actions,
+    );
   }
 }
