cb30fcd764c3d013e93e6a00419905d8d77d2734
Loren Aguey <loren@blackcatdev.io>
2023-05-05 09:16:46 -0400

    Replace connectivity_plus with internet_connection_checker in ConnectivityListener

--- lib/services/connectivity/connectivity_listener.dart
+++ lib/services/connectivity/connectivity_listener.dart
@@ -1,49 +1,41 @@
 import 'dart:async';
 
-import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:epic_skies/utils/logging/app_debug_log.dart';
+import 'package:internet_connection_checker/internet_connection_checker.dart';
 
 class ConnectivityListener {
   ConnectivityListener._();
 
   static bool hasConnection = false;
 
-  static late StreamSubscription<ConnectivityResult> _connectivitySubscription;
+  static late StreamSubscription<InternetConnectionStatus>
+      _connectivitySubscription;
 
   static void initConnectivityListener() {
     _updateConnectivity();
-    _connectivitySubscription = Connectivity()
-        .onConnectivityChanged
-        .listen((ConnectivityResult result) {
-      hasConnection = result != ConnectivityResult.none;
-      _logConnectivityListener(result: result);
+    _connectivitySubscription = InternetConnectionChecker()
+        .onStatusChange
+        .listen((InternetConnectionStatus result) {
+      hasConnection = result == InternetConnectionStatus.connected;
+      _logConnectivityListener(hasConnection);
     });
   }
 
   static void disposeConnectivityListener() {
     _connectivitySubscription.cancel();
-    _logConnectivityListener(message: 'ConnectivityListener disposed');
+    _logConnectivityListener(hasConnection);
   }
 
   static Future<void> _updateConnectivity() async {
-    final result = await Connectivity().checkConnectivity();
+    hasConnection = await InternetConnectionChecker().hasConnection;
 
-    hasConnection = result != ConnectivityResult.none;
-
-    _logConnectivityListener(result: result);
+    _logConnectivityListener(hasConnection);
   }
 
-  static void _logConnectivityListener({
-    ConnectivityResult? result,
-    String? message,
-  }) {
-    var logMessage = '';
-
-    if (result != null) {
-      logMessage = 'ConnectivityResult: $result';
-    } else {
-      logMessage = message!;
-    }
+  static void _logConnectivityListener(
+    bool result,
+  ) {
+    final logMessage = 'ConnectivityListener: hasConnection: $result';
 
     AppDebug.log(
       logMessage,
