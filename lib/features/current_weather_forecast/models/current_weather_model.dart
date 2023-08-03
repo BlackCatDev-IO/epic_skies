2705377e56e032d82bb9a2664800a0d78bfad643
Loren Aguey <loren@blackcatdev.io>
2023-08-02 20:17:24 -0400

    Update weather models with dart_mappable #62

--- lib/features/current_weather_forecast/models/current_weather_model.dart
+++ lib/features/current_weather_forecast/models/current_weather_model.dart
@@ -1,10 +1,20 @@
 import 'package:dart_mappable/dart_mappable.dart';
-import 'package:epic_skies/core/network/weather_kit/models/current/current_weather_data.dart';
+import 'package:epic_skies/features/main_weather/models/weather_response_model/current_data/current_data.dart';
 import 'package:epic_skies/services/settings/unit_settings/unit_settings_model.dart';
 import 'package:epic_skies/utils/conversions/unit_converter.dart';
+import 'package:epic_skies/utils/conversions/weather_code_converter.dart';
 
 part 'current_weather_model.mapper.dart';
 
+@MappableClass()
+class CurrentWeatherModel with CurrentWeatherModelMappable {
+  const CurrentWeatherModel({
+    required this.temp,
+    required this.feelsLike,
+    required this.windSpeed,
+    required this.condition,
+    required this.unitSettings,
+  });
 @MappableClass()
 class CurrentWeatherModel with CurrentWeatherModelMappable {
   const CurrentWeatherModel({
@@ -45,6 +55,7 @@ class CurrentWeatherModel with CurrentWeatherModelMappable {
         windSpeed: 0,
         condition: '',
         unitSettings: UnitSettings(),
+        unitSettings: UnitSettings(),
       );
 
   final int temp;
@@ -53,12 +64,23 @@ class CurrentWeatherModel with CurrentWeatherModelMappable {
   final String condition;
   final UnitSettings unitSettings;
 
-  static const fromMap = CurrentWeatherModelMapper.fromMap;
-}
-
-String _splitPascalCase(String input) {
-  if (input.isEmpty) {
-    return '';
+  static bool _isSnowyCondition(String condition) {
+    switch (condition.toLowerCase()) {
+      case 'snow':
+      case 'flurries':
+      case 'light snow':
+      case 'heavy snow':
+      case 'freezing drizzle':
+      case 'freezing rain':
+      case 'light freezing rain':
+      case 'heavy freezing rain':
+      case 'ice pellets':
+      case 'heavy ice pellets':
+      case 'light ice pellets':
+        return true;
+      default:
+        return false;
+    }
   }
 
   final stringBuffer = StringBuffer();
@@ -70,5 +92,5 @@ String _splitPascalCase(String input) {
     stringBuffer.write(input[i]);
   }
 
-  return stringBuffer.toString();
+  static const fromMap = CurrentWeatherModelMapper.fromMap;
 }
