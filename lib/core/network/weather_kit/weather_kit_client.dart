8885ad9d34279fa8ea59d5ca4aee14a55324c6a7
Loren Aguey <loren@blackcatdev.io>
2023-08-20 01:29:57 -0400

    Change JWT expire time to 1 hour #62

--- lib/core/network/weather_kit/weather_kit_client.dart
+++ lib/core/network/weather_kit/weather_kit_client.dart
@@ -33,7 +33,7 @@ class WeatherKitClient {
   String _token = '';
 
   late DateTime _tokenIssuedAt;
-  final Duration _tokenDuration = const Duration(seconds: 10);
+  final Duration _tokenDuration = const Duration(hours: 1);
 
   String _getJwt() {
     _tokenIssuedAt = DateTime.now();
