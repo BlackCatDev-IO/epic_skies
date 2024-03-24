32065afaf65b7b931fa266ce4b7de9f1a9a0f7ed
Loren Aguey <loren@blackcatdev.io>
2024-03-24 11:50:17 -0400

    Update app_update_bloc unit tests

--- test/services/app_updates/app_update_bloc_test.dart
+++ test/services/app_updates/app_update_bloc_test.dart
@@ -15,12 +15,21 @@ void main() async {
   late String mostRecentChanges;
   late String changeLog;
 
+  late String updateOnePoint5Message;
+
   setUpAll(() async {
     WidgetsFlutterBinding.ensureInitialized();
     appVersion = '1.0.0';
     updatedAppVersion = '1.1.0';
     mostRecentChanges = 'most_recent_changes';
     changeLog = 'change_log';
+
+    updateOnePoint5Message = '''
+Thanks for updating to 1.5.0! This update includes:
+ - Much improved weather accuracy with Apples WeatherKit (formerly Dark Sky) weather API
+ - Severe weather alerts and precipitation warnings on the home screen
+ - Fix for "EuropeKiev" timezone
+ ''';
 
     mockSystemInfo = MockSystemInfoRepo();
 
@@ -40,7 +49,13 @@ void main() async {
 emits `notUpdated` on first install with system info''',
       setUp: () {},
       build: () => AppUpdateBloc(systemInfo: mockSystemInfo),
-      act: (AppUpdateBloc bloc) => bloc.add(AppInitInfoOnAppStart()),
+      seed: AppUpdateState.firstInstall,
+      act: (AppUpdateBloc bloc) => bloc.add(
+        AppInitInfoOnAppStart(
+          minorVersionLowThreshold: 1,
+          minorVersionHighThreshold: 5,
+        ),
+      ),
       expect: () => <AppUpdateState>[
         AppUpdateState(
           currentAppVersion: appVersion,
@@ -62,17 +77,25 @@ emits `notUpdated` after first install when previous app version from storage eq
         updatedChanges: mostRecentChanges,
         status: AppUpdateStatus.notUpdated,
       ),
-      act: (AppUpdateBloc bloc) => bloc.add(AppInitInfoOnAppStart()),
+      act: (AppUpdateBloc bloc) => bloc.add(
+        AppInitInfoOnAppStart(
+          minorVersionLowThreshold: 1,
+          minorVersionHighThreshold: 5,
+        ),
+      ),
       expect: () => <AppUpdateState>[], // no changes to seeded state expected
     );
 
     blocTest<AppUpdateBloc, AppUpdateState>(
       '''
-emits `updated` after first install when system info returns different new version
+emits new app version number but still a `notUpdated` status when updated version
+doesn't meet threshold to show a dialog to the user
 ''',
       setUp: () {
         when(() => mockSystemInfo.currentAppVersion)
             .thenReturn(updatedAppVersion);
+        when(() => mockSystemInfo.mostRecentChanges)
+            .thenReturn(mostRecentChanges);
       },
       build: () => AppUpdateBloc(systemInfo: mockSystemInfo),
       seed: () => AppUpdateState(
@@ -81,13 +104,82 @@ emits `updated` after first install when system info returns different new versi
         updatedChanges: mostRecentChanges,
         status: AppUpdateStatus.notUpdated,
       ),
-      act: (AppUpdateBloc bloc) => bloc.add(AppInitInfoOnAppStart()),
+      act: (AppUpdateBloc bloc) => bloc.add(
+        AppInitInfoOnAppStart(
+          minorVersionLowThreshold: 5,
+          minorVersionHighThreshold: 5,
+        ),
+      ),
       expect: () => <AppUpdateState>[
         AppUpdateState(
           currentAppVersion: updatedAppVersion,
           changeLog: mockSystemInfo.changeLog,
           updatedChanges: mostRecentChanges,
-          status: AppUpdateStatus.updated,
+          status: AppUpdateStatus.notUpdated,
+        ),
+      ],
+    );
+
+    blocTest<AppUpdateBloc, AppUpdateState>(
+      '''
+emits `AppUpdateStatus.updated` && `update115Mesasage` when updating from 
+< 1.5.X to >= 1.5''',
+      setUp: () {
+        when(() => mockSystemInfo.currentAppVersion).thenReturn('1.5.0');
+        when(() => mockSystemInfo.mostRecentChanges)
+            .thenReturn(updateOnePoint5Message);
+      },
+      build: () => AppUpdateBloc(systemInfo: mockSystemInfo),
+      seed: () => AppUpdateState(
+        currentAppVersion: '1.1.2',
+        changeLog: mockSystemInfo.changeLog,
+        updatedChanges: mostRecentChanges,
+        status: AppUpdateStatus.notUpdated,
+      ),
+      act: (AppUpdateBloc bloc) => bloc.add(
+        AppInitInfoOnAppStart(
+          minorVersionLowThreshold: 1,
+          minorVersionHighThreshold: 5,
+        ),
+      ),
+      expect: () => <AppUpdateState>[
+        AppUpdateState(
+          currentAppVersion: '1.5.0',
+          changeLog: mockSystemInfo.changeLog,
+          updatedChanges: updateOnePoint5Message,
+          status: AppUpdateStatus.updatedShowUpdateDialog,
+        ),
+      ],
+    );
+
+    blocTest<AppUpdateBloc, AppUpdateState>(
+      '''
+emits `AppUpdateStatus.notUpdated` when `minorVersionHighThreshold` condition 
+isn't met''',
+      setUp: () {
+        when(() => mockSystemInfo.currentAppVersion).thenReturn('1.4.2');
+        when(() => mockSystemInfo.mostRecentChanges)
+            .thenReturn(mostRecentChanges);
+      },
+      build: () => AppUpdateBloc(systemInfo: mockSystemInfo),
+      seed: () => AppUpdateState(
+        currentAppVersion: '1.1.2',
+        changeLog: mockSystemInfo.changeLog,
+        updatedChanges: mostRecentChanges,
+        status: AppUpdateStatus.notUpdated,
+      ),
+      act: (AppUpdateBloc bloc) => bloc.add(
+        AppInitInfoOnAppStart(
+          minorVersionLowThreshold: 2,
+          minorVersionHighThreshold: 5,
+        ),
+      ),
+      expect: () => <AppUpdateState>[
+        AppUpdateState(
+          currentAppVersion: '1.4.2',
+          changeLog: mockSystemInfo.changeLog,
+          updatedChanges: mostRecentChanges,
+          status: AppUpdateStatus.notUpdated,
         ),
       ],
     );
