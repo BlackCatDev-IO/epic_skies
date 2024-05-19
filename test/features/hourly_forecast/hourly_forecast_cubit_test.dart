a74fc1298337c9b8d73143ca3ab7d687750524d7
Loren Aguey <loren@blackcatdev.io>
2024-05-19 14:20:48 -0400

    Add unit tests for expected Suntime insertions in HourlyForecastCubit #48

--- test/features/hourly_forecast/hourly_forecast_cubit_test.dart
+++ test/features/hourly_forecast/hourly_forecast_cubit_test.dart
@@ -1,12 +1,8 @@
 import 'package:bloc_test/bloc_test.dart';
 import 'package:epic_skies/features/hourly_forecast/cubit/hourly_forecast_cubit.dart';
-import 'package:epic_skies/utils/timezone/timezone_util.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:get_it/get_it.dart';
-import 'package:hydrated_bloc/hydrated_bloc.dart';
-import 'package:mocktail/mocktail.dart';
 
-import '../../mocks/mock_classes.dart';
+import '../../mocks/init_hydrated_storage.dart';
 import '../../mocks/visual_crossing_mocks/visual_crossing_nyc_metric.dart';
 import '../main_weather/mock_weather_state.dart';
 
@@ -14,19 +10,8 @@ class MockHourlyForecastCubit extends MockCubit<HourlyForecastState>
     implements HourlyForecastCubit {}
 
 void main() async {
-  late Storage storage;
-
-  setUpAll(() {
-    storage = MockHydratedStorage();
-    HydratedBloc.storage = storage;
-    when(
-      () => storage.write(any(), any<dynamic>()),
-    ).thenAnswer((_) async {});
-    HydratedBloc.storage = storage;
-
-    GetIt.I.registerSingleton(TimeZoneUtil());
-  });
-  group('HourlyForecastCubit:', () {
+  setUpAll(initHydratedStorage);
+  group('HourlyForecastCubit: Expected lengths', () {
     test(
       '2 Suntimes added to each block of 24 hours with WeatherKitAPI',
       () async {
@@ -133,5 +118,567 @@ void main() async {
         );
       },
     );
+  });
+
+  group(
+    '''Suntimes in expected locations with expected values with WeatherKit API''',
+    () {
+      test(
+        'Next24Hours',
+        () async {
+          final hourlyCubit = HourlyForecastCubit()
+            ..refreshHourlyData(
+              updatedWeatherState: MockWeatherState().mockWeatherKitState(),
+            );
+
+          const expectedSunriseIndex = 20;
+          const expectedSunsetIndex = 8;
+
+          expect(
+            hourlyCubit.state.next24Hours[expectedSunriseIndex].isSunrise,
+            true,
+          );
+
+          expect(
+            hourlyCubit.state.next24Hours[expectedSunriseIndex].suntimeString !=
+                null,
+            true,
+          );
+
+          expect(
+            hourlyCubit.state.next24Hours[expectedSunsetIndex].isSunrise,
+            false,
+          );
+
+          expect(
+            hourlyCubit.state.next24Hours[expectedSunsetIndex].suntimeString !=
+                null,
+            true,
+          );
+        },
+      );
+
+      test(
+        'Day 1',
+        () async {
+          final hourlyCubit = HourlyForecastCubit()
+            ..refreshHourlyData(
+              updatedWeatherState: MockWeatherState().mockWeatherKitState(),
+            );
+
+          const expectedSunriseIndex = 7;
+          const expectedSunsetIndex = 21;
+
+          expect(
+            hourlyCubit.state.day1[expectedSunriseIndex].isSunrise,
+            true,
+          );
+
+          expect(
+            hourlyCubit.state.day1[expectedSunriseIndex].suntimeString != null,
+            true,
+          );
+
+          expect(
+            hourlyCubit.state.day1[expectedSunsetIndex].isSunrise,
+            false,
+          );
+
+          expect(
+            hourlyCubit.state.day1[expectedSunsetIndex].suntimeString != null,
+            true,
+          );
+        },
+      );
+
+      test('Day 2', () async {
+        final hourlyCubit = HourlyForecastCubit()
+          ..refreshHourlyData(
+            updatedWeatherState: MockWeatherState().mockWeatherKitState(),
+          );
+        const expectedSunriseIndex = 7;
+        const expectedSunsetIndex = 21;
+        expect(
+          hourlyCubit.state.day2[expectedSunriseIndex].isSunrise,
+          true,
+        );
+        expect(
+          hourlyCubit.state.day2[expectedSunriseIndex].suntimeString != null,
+          true,
+        );
+        expect(
+          hourlyCubit.state.day2[expectedSunsetIndex].isSunrise,
+          false,
+        );
+        expect(
+          hourlyCubit.state.day2[expectedSunsetIndex].suntimeString != null,
+          true,
+        );
+      });
+
+      test('Day 3', () async {
+        final hourlyCubit = HourlyForecastCubit()
+          ..refreshHourlyData(
+            updatedWeatherState: MockWeatherState().mockWeatherKitState(),
+          );
+        const expectedSunriseIndex = 7;
+        const expectedSunsetIndex = 21;
+        expect(
+          hourlyCubit.state.day3[expectedSunriseIndex].isSunrise,
+          true,
+        );
+        expect(
+          hourlyCubit.state.day3[expectedSunriseIndex].suntimeString != null,
+          true,
+        );
+        expect(
+          hourlyCubit.state.day3[expectedSunsetIndex].isSunrise,
+          false,
+        );
+        expect(
+          hourlyCubit.state.day3[expectedSunsetIndex].suntimeString != null,
+          true,
+        );
+      });
+
+      test('Day 4', () async {
+        final hourlyCubit = HourlyForecastCubit()
+          ..refreshHourlyData(
+            updatedWeatherState: MockWeatherState().mockWeatherKitState(),
+          );
+        const expectedSunriseIndex = 7;
+        const expectedSunsetIndex = 21;
+        expect(
+          hourlyCubit.state.day4[expectedSunriseIndex].isSunrise,
+          true,
+        );
+        expect(
+          hourlyCubit.state.day4[expectedSunriseIndex].suntimeString != null,
+          true,
+        );
+        expect(
+          hourlyCubit.state.day4[expectedSunsetIndex].isSunrise,
+          false,
+        );
+        expect(
+          hourlyCubit.state.day4[expectedSunsetIndex].suntimeString != null,
+          true,
+        );
+      });
+
+      test('Day 5', () async {
+        final hourlyCubit = HourlyForecastCubit()
+          ..refreshHourlyData(
+            updatedWeatherState: MockWeatherState().mockWeatherKitState(),
+          );
+        const expectedSunriseIndex = 7;
+        const expectedSunsetIndex = 21;
+        expect(
+          hourlyCubit.state.day5[expectedSunriseIndex].isSunrise,
+          true,
+        );
+        expect(
+          hourlyCubit.state.day5[expectedSunriseIndex].suntimeString != null,
+          true,
+        );
+        expect(
+          hourlyCubit.state.day5[expectedSunsetIndex].isSunrise,
+          false,
+        );
+        expect(
+          hourlyCubit.state.day5[expectedSunsetIndex].suntimeString != null,
+          true,
+        );
+      });
+
+      test('Day 6', () async {
+        final hourlyCubit = HourlyForecastCubit()
+          ..refreshHourlyData(
+            updatedWeatherState: MockWeatherState().mockWeatherKitState(),
+          );
+        const expectedSunriseIndex = 7;
+        const expectedSunsetIndex = 21;
+        expect(
+          hourlyCubit.state.day6[expectedSunriseIndex].isSunrise,
+          true,
+        );
+        expect(
+          hourlyCubit.state.day6[expectedSunriseIndex].suntimeString != null,
+          true,
+        );
+        expect(
+          hourlyCubit.state.day6[expectedSunsetIndex].isSunrise,
+          false,
+        );
+        expect(
+          hourlyCubit.state.day6[expectedSunsetIndex].suntimeString != null,
+          true,
+        );
+      });
+
+      test('Day 7', () async {
+        final hourlyCubit = HourlyForecastCubit()
+          ..refreshHourlyData(
+            updatedWeatherState: MockWeatherState().mockWeatherKitState(),
+          );
+        const expectedSunriseIndex = 7;
+        const expectedSunsetIndex = 21;
+        expect(
+          hourlyCubit.state.day7[expectedSunriseIndex].isSunrise,
+          true,
+        );
+        expect(
+          hourlyCubit.state.day7[expectedSunriseIndex].suntimeString != null,
+          true,
+        );
+        expect(
+          hourlyCubit.state.day7[expectedSunsetIndex].isSunrise,
+          false,
+        );
+        expect(
+          hourlyCubit.state.day7[expectedSunsetIndex].suntimeString != null,
+          true,
+        );
+      });
+
+      test('Day 8', () async {
+        final hourlyCubit = HourlyForecastCubit()
+          ..refreshHourlyData(
+            updatedWeatherState: MockWeatherState().mockWeatherKitState(),
+          );
+        const expectedSunriseIndex = 7;
+        const expectedSunsetIndex = 21;
+        expect(
+          hourlyCubit.state.day8[expectedSunriseIndex].isSunrise,
+          true,
+        );
+        expect(
+          hourlyCubit.state.day8[expectedSunriseIndex].suntimeString != null,
+          true,
+        );
+        expect(
+          hourlyCubit.state.day8[expectedSunsetIndex].isSunrise,
+          false,
+        );
+        expect(
+          hourlyCubit.state.day8[expectedSunsetIndex].suntimeString != null,
+          true,
+        );
+      });
+
+      test('Day 9', () async {
+        final hourlyCubit = HourlyForecastCubit()
+          ..refreshHourlyData(
+            updatedWeatherState: MockWeatherState().mockWeatherKitState(),
+          );
+        const expectedSunriseIndex = 7;
+        const expectedSunsetIndex = 21;
+        expect(
+          hourlyCubit.state.day9[expectedSunriseIndex].isSunrise,
+          true,
+        );
+        expect(
+          hourlyCubit.state.day9[expectedSunriseIndex].suntimeString != null,
+          true,
+        );
+        expect(
+          hourlyCubit.state.day9[expectedSunsetIndex].isSunrise,
+          false,
+        );
+        expect(
+          hourlyCubit.state.day9[expectedSunsetIndex].suntimeString != null,
+          true,
+        );
+      });
+    },
+  );
+
+  group(
+      '''Suntimes in expected locations with expected values with VisualCrossing API''',
+      () {
+    test(
+      'Next24Hours',
+      () async {
+        final hourlyCubit = HourlyForecastCubit()
+          ..refreshHourlyData(
+            updatedWeatherState: MockWeatherState()
+                .mockVisualCrossingState(nycVisualCrossingMetric),
+          );
+
+        const expectedSunriseIndex = 17;
+        const expectedSunsetIndex = 7;
+
+        expect(
+          hourlyCubit.state.next24Hours[expectedSunriseIndex].isSunrise,
+          true,
+        );
+
+        expect(
+          hourlyCubit.state.next24Hours[expectedSunriseIndex].suntimeString !=
+              null,
+          true,
+        );
+
+        expect(
+          hourlyCubit.state.next24Hours[expectedSunsetIndex].isSunrise,
+          false,
+        );
+
+        expect(
+          hourlyCubit.state.next24Hours[expectedSunsetIndex].suntimeString !=
+              null,
+          true,
+        );
+      },
+    );
+    test(
+      'Day 1',
+      () async {
+        final hourlyCubit = HourlyForecastCubit()
+          ..refreshHourlyData(
+            updatedWeatherState: MockWeatherState()
+                .mockVisualCrossingState(nycVisualCrossingMetric),
+          );
+
+        const expectedSunriseIndex = 6;
+        const expectedSunsetIndex = 22;
+
+        expect(
+          hourlyCubit.state.day1[expectedSunriseIndex].isSunrise,
+          true,
+        );
+
+        expect(
+          hourlyCubit.state.day1[expectedSunriseIndex].suntimeString != null,
+          true,
+        );
+
+        expect(
+          hourlyCubit.state.day1[expectedSunsetIndex].isSunrise,
+          false,
+        );
+
+        expect(
+          hourlyCubit.state.day1[expectedSunsetIndex].suntimeString != null,
+          true,
+        );
+      },
+    );
+
+    test(
+      'Day 2',
+      () async {
+        final hourlyCubit = HourlyForecastCubit()
+          ..refreshHourlyData(
+            updatedWeatherState: MockWeatherState()
+                .mockVisualCrossingState(nycVisualCrossingMetric),
+          );
+
+        const expectedSunriseIndex = 6;
+        const expectedSunsetIndex = 22;
+
+        expect(
+          hourlyCubit.state.day2[expectedSunriseIndex].isSunrise,
+          true,
+        );
+
+        expect(
+          hourlyCubit.state.day2[expectedSunriseIndex].suntimeString != null,
+          true,
+        );
+
+        expect(
+          hourlyCubit.state.day2[expectedSunsetIndex].isSunrise,
+          false,
+        );
+
+        expect(
+          hourlyCubit.state.day2[expectedSunsetIndex].suntimeString != null,
+          true,
+        );
+      },
+    );
+
+    test('Day 3', () async {
+      final hourlyCubit = HourlyForecastCubit()
+        ..refreshHourlyData(
+          updatedWeatherState: MockWeatherState()
+              .mockVisualCrossingState(nycVisualCrossingMetric),
+        );
+      const expectedSunriseIndex = 6;
+      const expectedSunsetIndex = 22;
+      expect(
+        hourlyCubit.state.day3[expectedSunriseIndex].isSunrise,
+        true,
+      );
+      expect(
+        hourlyCubit.state.day3[expectedSunriseIndex].suntimeString != null,
+        true,
+      );
+      expect(
+        hourlyCubit.state.day3[expectedSunsetIndex].isSunrise,
+        false,
+      );
+      expect(
+        hourlyCubit.state.day3[expectedSunsetIndex].suntimeString != null,
+        true,
+      );
+    });
+
+    test('Day 4', () async {
+      final hourlyCubit = HourlyForecastCubit()
+        ..refreshHourlyData(
+          updatedWeatherState: MockWeatherState()
+              .mockVisualCrossingState(nycVisualCrossingMetric),
+        );
+      const expectedSunriseIndex = 6;
+      const expectedSunsetIndex = 22;
+      expect(
+        hourlyCubit.state.day4[expectedSunriseIndex].isSunrise,
+        true,
+      );
+      expect(
+        hourlyCubit.state.day4[expectedSunriseIndex].suntimeString != null,
+        true,
+      );
+      expect(
+        hourlyCubit.state.day4[expectedSunsetIndex].isSunrise,
+        false,
+      );
+      expect(
+        hourlyCubit.state.day4[expectedSunsetIndex].suntimeString != null,
+        true,
+      );
+    });
+
+    test('Day 5', () async {
+      final hourlyCubit = HourlyForecastCubit()
+        ..refreshHourlyData(
+          updatedWeatherState: MockWeatherState()
+              .mockVisualCrossingState(nycVisualCrossingMetric),
+        );
+      const expectedSunriseIndex = 6;
+      const expectedSunsetIndex = 22;
+      expect(
+        hourlyCubit.state.day5[expectedSunriseIndex].isSunrise,
+        true,
+      );
+      expect(
+        hourlyCubit.state.day5[expectedSunriseIndex].suntimeString != null,
+        true,
+      );
+      expect(
+        hourlyCubit.state.day5[expectedSunsetIndex].isSunrise,
+        false,
+      );
+      expect(
+        hourlyCubit.state.day5[expectedSunsetIndex].suntimeString != null,
+        true,
+      );
+    });
+
+    test('Day 6', () async {
+      final hourlyCubit = HourlyForecastCubit()
+        ..refreshHourlyData(
+          updatedWeatherState: MockWeatherState()
+              .mockVisualCrossingState(nycVisualCrossingMetric),
+        );
+      const expectedSunriseIndex = 6;
+      const expectedSunsetIndex = 22;
+      expect(
+        hourlyCubit.state.day6[expectedSunriseIndex].isSunrise,
+        true,
+      );
+      expect(
+        hourlyCubit.state.day6[expectedSunriseIndex].suntimeString != null,
+        true,
+      );
+      expect(
+        hourlyCubit.state.day6[expectedSunsetIndex].isSunrise,
+        false,
+      );
+      expect(
+        hourlyCubit.state.day6[expectedSunsetIndex].suntimeString != null,
+        true,
+      );
+    });
+
+    test('Day 7', () async {
+      final hourlyCubit = HourlyForecastCubit()
+        ..refreshHourlyData(
+          updatedWeatherState: MockWeatherState()
+              .mockVisualCrossingState(nycVisualCrossingMetric),
+        );
+      const expectedSunriseIndex = 6;
+      const expectedSunsetIndex = 22;
+      expect(
+        hourlyCubit.state.day7[expectedSunriseIndex].isSunrise,
+        true,
+      );
+      expect(
+        hourlyCubit.state.day7[expectedSunriseIndex].suntimeString != null,
+        true,
+      );
+      expect(
+        hourlyCubit.state.day7[expectedSunsetIndex].isSunrise,
+        false,
+      );
+      expect(
+        hourlyCubit.state.day7[expectedSunsetIndex].suntimeString != null,
+        true,
+      );
+    });
+
+    test('Day 8', () async {
+      final hourlyCubit = HourlyForecastCubit()
+        ..refreshHourlyData(
+          updatedWeatherState: MockWeatherState()
+              .mockVisualCrossingState(nycVisualCrossingMetric),
+        );
+      const expectedSunriseIndex = 6;
+      const expectedSunsetIndex = 22;
+      expect(
+        hourlyCubit.state.day8[expectedSunriseIndex].isSunrise,
+        true,
+      );
+      expect(
+        hourlyCubit.state.day8[expectedSunriseIndex].suntimeString != null,
+        true,
+      );
+      expect(
+        hourlyCubit.state.day8[expectedSunsetIndex].isSunrise,
+        false,
+      );
+      expect(
+        hourlyCubit.state.day8[expectedSunsetIndex].suntimeString != null,
+        true,
+      );
+    });
+
+    test('Day 9', () async {
+      final hourlyCubit = HourlyForecastCubit()
+        ..refreshHourlyData(
+          updatedWeatherState: MockWeatherState()
+              .mockVisualCrossingState(nycVisualCrossingMetric),
+        );
+      const expectedSunriseIndex = 6;
+      const expectedSunsetIndex = 22;
+      expect(
+        hourlyCubit.state.day9[expectedSunriseIndex].isSunrise,
+        true,
+      );
+      expect(
+        hourlyCubit.state.day9[expectedSunriseIndex].suntimeString != null,
+        true,
+      );
+      expect(
+        hourlyCubit.state.day9[expectedSunsetIndex].isSunrise,
+        false,
+      );
+      expect(
+        hourlyCubit.state.day9[expectedSunsetIndex].suntimeString != null,
+        true,
+      );
+    });
   });
 }
