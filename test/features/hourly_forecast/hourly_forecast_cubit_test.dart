cc0f655df1a8b252b1fee66ec981076bfb8981bc
Loren Aguey <loren@blackcatdev.io>
2024-05-19 13:56:19 -0400

    HourlyForecastCubit add expected length test for backup api #48

--- test/features/hourly_forecast/hourly_forecast_cubit_test.dart
+++ test/features/hourly_forecast/hourly_forecast_cubit_test.dart
@@ -7,6 +7,7 @@ import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:mocktail/mocktail.dart';
 
 import '../../mocks/mock_classes.dart';
+import '../../mocks/visual_crossing_mocks/visual_crossing_nyc_metric.dart';
 import '../main_weather/mock_weather_state.dart';
 
 class MockHourlyForecastCubit extends MockCubit<HourlyForecastState>
@@ -27,13 +28,67 @@ void main() async {
   });
   group('HourlyForecastCubit:', () {
     test(
-      '2 Suntimes added to each block of 24 hours',
+      '2 Suntimes added to each block of 24 hours with WeatherKitAPI',
       () async {
         final hourlyCubit = HourlyForecastCubit()
           ..refreshHourlyData(
             updatedWeatherState: MockWeatherState().mockWeatherKitState(),
           );
-          
+
+        const expectedLengthWithSunTimes = 26;
+
+        expect(
+          hourlyCubit.state.next24Hours.length,
+          expectedLengthWithSunTimes,
+        );
+        expect(
+          hourlyCubit.state.day1.length,
+          expectedLengthWithSunTimes,
+        );
+        expect(
+          hourlyCubit.state.day2.length,
+          expectedLengthWithSunTimes,
+        );
+        expect(
+          hourlyCubit.state.day3.length,
+          expectedLengthWithSunTimes,
+        );
+        expect(
+          hourlyCubit.state.day4.length,
+          expectedLengthWithSunTimes,
+        );
+        expect(
+          hourlyCubit.state.day5.length,
+          expectedLengthWithSunTimes,
+        );
+        expect(
+          hourlyCubit.state.day6.length,
+          expectedLengthWithSunTimes,
+        );
+        expect(
+          hourlyCubit.state.day7.length,
+          expectedLengthWithSunTimes,
+        );
+        expect(
+          hourlyCubit.state.day8.length,
+          expectedLengthWithSunTimes,
+        );
+        expect(
+          hourlyCubit.state.day9.length,
+          expectedLengthWithSunTimes,
+        );
+      },
+    );
+
+    test(
+      '2 Suntimes added to each block of 24 hours with VisualCrossingAPI',
+      () async {
+        final hourlyCubit = HourlyForecastCubit()
+          ..refreshHourlyData(
+            updatedWeatherState: MockWeatherState()
+                .mockVisualCrossingState(nycVisualCrossingMetric),
+          );
+
         const expectedLengthWithSunTimes = 26;
 
         expect(
