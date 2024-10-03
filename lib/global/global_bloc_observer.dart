5e63ded8f5303612902baf93c1e2ff70d067cfe2
Loren Aguey <loren@blackcatdev.io>
2024-10-03 17:01:00 -0400

    Add change logging to Cubit classes

--- lib/global/global_bloc_observer.dart
+++ lib/global/global_bloc_observer.dart
@@ -37,6 +37,12 @@ class GlobalBlocObserver extends BlocObserver {
   }
 
   @override
+  void onChange(BlocBase<dynamic> bloc, Change<dynamic> change) {
+    super.onChange(bloc, change);
+    AppDebug.logCubitChange(change, '${bloc.runtimeType}');
+  }
+
+  @override
   void onError(BlocBase<dynamic> bloc, Object error, StackTrace stackTrace) {
     super.onError(bloc, error, stackTrace);
 
